6f1f65f289660446b72026409d624f9c9255d7c4
Lars Wagner <lars@tubies.de>
2024-02-03 19:58:35 +0100

    refactor: refactoring todo app

--- script.js
+++ script.js
@@ -1,72 +1,74 @@
 "use strict";
 
-/* SETTING UP REFERENCES ======================================================== */
-const taskForm = document.getElementById("task-form");
-const input = document.getElementById("input");
-const taskList = document.getElementById("task-list");
-const clearButton = document.getElementById("clear-btn");
-const checkboxes = document.querySelectorAll(".toggle-complete");
-const claim = document.querySelector(".claim");
+/*    SETTING UP DOM ELEMENTS
+========================================================================== */
+const taskForm = document.getElementById("task-form"); // reference to form
+const input = document.getElementById("input"); // reference to inputfield
+const taskList = document.getElementById("task-list"); // reference to task list
+const clearButton = document.getElementById("clear-btn"); // reference to clear button
+const checkboxes = document.querySelectorAll(".toggle-complete"); // get all checkboxes
 
-/* SETTING UP STATE ============================================================= */
+/* API ELEMENTS */
+const apiUrl = "http://localhost:4730/todos";
+
+/*    SETTING UP STATE
+========================================================================== */
 const state = {
   currentFilter: "all", // "all", "done", "open"
   tasks: [],
 };
 
-// Add a default "testTodo" if there are no tasks in localStorage
-if (state.tasks.length === 0 && localStorage.getItem("tasks") === null) {
-  const testTodo = {
-    id: new Date().getTime(),
-    description: "Start your first task 😜",
-    isDone: false,
-  };
-  state.tasks.push(testTodo);
-  saveTodosToApi();
-}
+initialize();
 
 /* FUNCTION - LOAD TODOS FROM API ====================================== */
 
 function getTodosFromApi() {
-  //state.tasks = JSON.parse(localStorage.getItem("tasks")) ?? [];
-
-  fetch("http://localhost:4730/todos")
-    .then((res) => res.json())
+  state.tasks = JSON.parse(localStorage.getItem("tasks")) ?? []; // load tasks from localStorage OR set a new array
+  /* fetch(apiUrl)
+    .then((response) => response.json())
     .then((todosFromApi) => {
       state.tasks = todosFromApi;
-      console.log(state.tasks);
+      //console.log(state.tasks);
       render();
-    });
+    })
+    .catch((error) => {
+      console.error("Error getting todos from API:", error);
+    }); */
 }
 
-/* FUNCTION - SAVE TODOS TO API ======================================== */
-
+/*    FUNCTION - SAVE TODOS TO LOCAL STORAGE
+========================================================================== */
 function saveTodosToApi() {
-  localStorage.setItem("tasks", JSON.stringify(state.tasks));
-  // Additional code for saving to external API, if needed
-  /*fetch("http://localhost:4730/todos", {
+  localStorage.setItem("tasks", JSON.stringify(state.tasks)); // save to localStorage
+
+  /*   const newTodoText = "test";
+  const newTodo = {
+    description: newTodoText,
+    done: false,
+  }; */
+  /* fetch(apiUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(state.tasks),
+    body: JSON.stringify(task),
   })
     .then((res) => res.json())
     .then((newTodoFromApi) => {
-      //todos.push(state.tasks);
-      state.tasks = newTodoFromApi;
-      //render();
+      state.tasks.push(input.value);
+      render();
     })
     .catch((error) => {
-      console.error("Error saving todos to API:", error);
-      // Handle errors as needed
-    });*/
+      console.error("Error saving todo to API:", error);
+    }); */
 }
 
-/* FUNCTION - GENERATE HTML TEMPLATE ============================================ */
+/*    FUNCTION - GENERATE HTML TEMPLATE
+========================================================================== */
 function generateTodoItemTemplate(task) {
   const li = document.createElement("li");
   li.classList.add("task-item");
+  li.id = "task-" + task.id; // Setzen Sie die ID für das li-Element
 
   const div = document.createElement("div");
   div.classList.add("checkbox-wrapper");
@@ -76,8 +78,9 @@ function generateTodoItemTemplate(task) {
   checkbox.id = "checkbox-toogle-" + task.id;
   checkbox.classList.add("toggle-complete");
 
-  checkbox.checked = task.isDone;
+  checkbox.checked = task.isDone; // set "checked" attribut based on task.isDone
 
+  // Event-Listener für das Ändern des Checkbox-Status hinzufügen
   checkbox.addEventListener("change", function () {
     task.isDone = checkbox.checked;
 
@@ -104,127 +107,74 @@ function generateTodoItemTemplate(task) {
   return li;
 }
 
-/* EVENT LISTENER - SUBMIT BUTTON =============================================== */
+/*    EVENT LISTENER - SUBMIT BUTTON
+// ========================================================================== */
 taskForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  // Check for duplicates, if there are some tasks which
   const isDuplicate = state.tasks.some(
     (task) =>
-      task.description.toLowerCase() === input.value.trim().toLowerCase()
+      task.description.toLowerCase() === input.value.trim().toLowerCase() // check if task in array === inputValue
   );
 
   if (isDuplicate) {
     alert("Sorry, you can't add a duplicate task!");
   } else {
     if (input.value.trim() !== "") {
+      // if field is NOT clear update state
       state.tasks.push({
-        //id: new Date().getTime(),
-        description: input.value.trim(),
-        isDone: false,
+        id: new Date().getTime(), // get time for create an id
+        description: input.value.trim(), // get input field, trim whitespaces before and after
+        isDone: false, // set complete status to false
       });
 
+      // Render state
       render();
+      saveTodosToApi();
 
-      input.value = "";
-      input.focus();
+      input.value = ""; // clear input field
+      input.focus(); // focus on input field for the next task
     } else {
-      return;
+      return; // do nothing
     }
   }
 });
 
-/* EVENT LISTENER - FILTERING ================================================== */
-const filterRadios = document.querySelectorAll('input[name="filter"]');
+/*    EVENT LISTENER - FILTERING
+========================================================================== */
+const filterRadios = document.querySelectorAll('input[name="filter"]'); // select radio inputs
 filterRadios.forEach((radio) => {
   radio.addEventListener("change", function () {
-    state.currentFilter = this.value;
+    state.currentFilter = this.value; // set currentfilter to this value
     render();
   });
 });
 
-/* EVENT LISTENER - CLEAR DONE TASKS ============================================ */
+/*    EVENT LISTENER - CLEAR DONE TASKS
+========================================================================== */
 clearButton.addEventListener("click", function () {
   if (confirm("Do you really want to delete the complete list?")) {
     state.tasks = state.tasks.filter((task) => !task.isDone);
-    saveTodosToApi();
+    saveTodosToApi(); // save to localStorage after filtering
     render();
   }
 });
 
-/* FUNCTION - DISPLAY =========================================================== */
-function display() {
-  taskList.innerHTML = "";
-
-  const filteredTodos = state.tasks.filter((task) => {
-    if (state.currentFilter == "done") {
-      return task.isDone;
-    }
-    if (state.currentFilter == "open") {
-      return !task.isDone;
-    }
-    return true;
-  });
-
-  filteredTodos.forEach(function (task) {
-    const listItem = generateTodoItemTemplate(task);
-    taskList.appendChild(listItem);
-  });
-
-  const checkboxes = document.querySelectorAll(".toggle-complete");
-  checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener("change", () => {
-      const taskId = parseInt(checkbox.id.replace("checkbox-", ""));
-      const currentTask = state.tasks.find((task) => task.id === taskId);
-
-      currentTask.isDone = checkbox.checked;
-      onStateChange();
-    });
-  });
-
-  const deleteButtons = document.querySelectorAll(".delete-task");
-  deleteButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const taskId = button.id;
-      state.tasks = state.tasks.filter(
-        (removeTask) => removeTask.id !== parseInt(taskId)
-      );
-      saveTodosToApi();
-      render();
-      document.getElementById(taskId).remove();
-    });
-  });
-}
-
-/* FUNCTION - ON STAGE (SAVE + DISPLAY WRAPPER) ================================ */
-function onStateChange() {
-  localStorage.setItem("tasks", JSON.stringify(state.tasks));
-  display();
-  currentTaskCount();
-}
-
-/* COUNT OPEN TASK - FUNCTION =================================================== */
-function currentTaskCount() {
-  const openTasks = state.tasks.filter((task) => !task.isDone);
-
-  if (openTasks.length > 0) {
-    claim.textContent = `Getting ${openTasks.length} things done…`;
-  } else {
-    claim.textContent = `Great, all tasks are completed!`;
-  }
-}
-
-/* FUNCTION - RENDER DATA FROM STATE =========================================== */
+/*    FUNCTION - RENDER DATA FROM STATE
+========================================================================== */
 function render() {
   taskList.innerHTML = "";
 
+  // Filtering
   const filteredTodos = state.tasks.filter((task) => {
     if (state.currentFilter == "done") {
-      return task.isDone;
+      return task.isDone; // set task to isDone
     }
     if (state.currentFilter == "open") {
-      return !task.isDone;
+      return !task.isDone; // set task to NOT isDone
     }
-    return true;
+    return true; // unfiltered tasks
   });
 
   for (const task of filteredTodos) {
@@ -232,29 +182,42 @@ function render() {
     taskList.appendChild(newTodoItem);
   }
 
+  /*    EVENT-LISTENER - REMOVE BUTTON
+========================================================================== */
   const deleteButtons = document.querySelectorAll(".delete-task");
   deleteButtons.forEach((button) => {
     button.addEventListener("click", () => {
-      const buttonId = button.id;
-      const taskId = buttonId.replace("delete-btn-", "");
+      const buttonId = button.id; // save the button ID
+      const taskId = buttonId.replace("delete-btn-", ""); // extract the ID
+
+      // Filter out the task with the specified ID from the state
       state.tasks = state.tasks.filter(
         (removeTask) => removeTask.id !== parseInt(taskId)
       );
-      saveTodosToApi();
+      saveTodosToApi(); // Save changes to the API
       render();
-      const listItem = document.getElementById(taskId).closest(".task-item");
-      listItem.remove();
+
+      const listItem = document.getElementById("task-" + taskId);
+      listItem.remove(); // remove the entire <li> element
     });
   });
-
-  saveTodosToApi();
 }
 
-/* FUNCTION - GET TODOS AND RENDER ============================================ */
+/*    FUNCTION - GET TODOS AND RENDER
+========================================================================== */
 function initialize() {
   getTodosFromApi();
-  render();
-  currentTaskCount();
-}
 
-initialize();
+  // Add a default "placeholderTodo" if there are no tasks in localStorage
+  if (state.tasks.length === 0 && localStorage.getItem("tasks") === null) {
+    const placeholderTodo = {
+      id: new Date().getTime(),
+      description: "Start your first task 😜",
+      isDone: false,
+    };
+    state.tasks.push(placeholderTodo);
+    saveTodosToApi();
+  }
+
+  render();
+}
